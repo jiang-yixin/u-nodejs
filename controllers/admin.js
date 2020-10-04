c65b9836cd626d6e2dace8f5ea2c19cc75d7e744
Yixin Jiang <jiangyixin0910@gmail.com>
2020-10-04 23:29:44 +0200

    Async requests from client side

--- controllers/admin.js
+++ controllers/admin.js
@@ -186,8 +186,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(prod => {
       if (!prod) {
@@ -198,11 +198,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(result => {
       console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
+      res.status(200).json({ message: 'deleted' });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(err);
+      res.status(500).json({ message: 'error occured' });
     });
 };
